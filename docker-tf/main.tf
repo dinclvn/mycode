add6e03c49d123db8ca7d4b33bca1a4734488e73
dinclvn <dinclvn@gmail.com>
2022-09-19 16:09:33 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,31 @@
+# sample comment for my main tf file
+/*here is a block of comment
+that keeps on going */
+
+#tf block
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+#providers
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true    // keep image after "destroy"
+}
+
+#resources
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
