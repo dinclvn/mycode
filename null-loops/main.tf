febec1e1acd6bad9cb2500702aa1946d57a5933b
dinclvn <dinclvn@gmail.com>
2022-09-20 19:04:39 +0000

    local-exec

--- null-loops/main.tf
+++ null-loops/main.tf
@@ -4,19 +4,32 @@
 
 /* a list of local variables */
 locals {
-  avengers = ["the wasp", "ant man", "ironman", "captain america", "thor", "doctor strange", "spider man", "hulk", "black panther", "black widow"]
+  avengers = {
+               "Iron Man"= {
+                "power"= "money"
+                "enemy"= "Iron Monger"}
+               "Black Panther"= {
+                "power"= "vibranium suit"
+                "enemy"= "War Monger"}
+               "She-Hulk"= {
+                "power"= "super strength"
+                "enemy"= "Abomination"}
+             }
 }
 
 
 /* The null_resource implements the standard resource lifecycle but takes no more action */
 resource "null_resource" "avengers" {
-  for_each = toset(local.avengers)
-  /* triggers allows specifying a random set of values that when
+  for_each = tomap(local.avengers)
+
+/* triggers allows specifying a random set of values that when
      changed will cause the resource to be replaced */
-  triggers = {
-    name = each.value  // a special variable, "each" created by terraform
-                       // the object has "each.key" and "each.value"
-  }
+
+triggers = {
+    name = each.key  // a special variable, "each" created by terraform
+    power = each.value.power
+    enemy = each.value.enemy
+}
 }
 
 /* We want these outputs */
